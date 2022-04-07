b12d53d2258940f3797d6f4df5f3251c324226b5
Kai Hendry <hendry@iki.fi>
2022-04-07 15:04:54 +0800

    awsContext not needed

--- handler.js
+++ handler.js
@@ -1,19 +1,48 @@
 "use strict";
+const axios = require("axios");
+const middy = require("@middy/core");
 
-const AWS = require("aws-sdk");
+function errorLoggerMiddleware() {
+  const errorLoggerMiddlewareOnError = async (request) => {
+    console.warn("context", request?.context);
+    console.warn(request.error.toJSON());
+    if (request.error.response) {
+      console.error("axios error", {
+        statusCode: request.error.response.status,
+        data: request.error.response.data,
+        functionName: request?.context.functionName,
+        hostname: new URL(request.error.config.url).hostname,
+        _aws: {
+          Timestamp: new Date().getTime(),
+          CloudWatchMetrics: [
+            {
+              Namespace: "backend",
+              Dimensions: [["hostname", "functionName"]],
+              Metrics: [
+                {
+                  Name: "statusCode",
+                },
+              ],
+            },
+          ],
+        },
+      });
+    }
+  };
+  return {
+    onError: errorLoggerMiddlewareOnError,
+  };
+}
 
-module.exports.start = async (event) => {
-  const lambda = new AWS.Lambda();
-  const resp = await lambda
-    .invoke({
-      FunctionName: "hendry-lambda-invoke-test-dev-invokeMEpls",
-      Payload: JSON.stringify({ foo: "bar" }),
-    })
-    .promise();
-  console.log("response from calling lambda", resp);
+const baseHandler = async (event) => {
+  resp = await axios.get("https://httpstat.us/404");
+  return { resp };
 };
 
-module.exports.invokeme = async (event) => {
-  console.log(event);
-  return { hello: event };
+const start = middy(baseHandler).use(
+  errorLoggerMiddleware()
+  // errorLoggerMiddleware({ awsContext: true })
+);
+module.exports = {
+  start,
 };
